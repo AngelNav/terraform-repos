b9abb2e4cecb5a98e1ac6bca8960c0f40e6e92e3
AngelNav <angel98.nava@gmail.com>
2023-11-16 00:22:00 -0600

    locals added to read json file

--- modules/repository/main.tf
+++ modules/repository/main.tf
@@ -2,8 +2,17 @@ provider "github" {
   token = var.github_token
 }
 
+locals {
+  #get json
+  repo_data_map = { 
+    for repo in jsondecode(file(var.repos_file)).repositories : 
+    repo.name => repo 
+  }
+}
+
+
 resource "github_repository" "repository" {
-  for_each = jsondecode(file(var.repos_file))
+  for_each = local.repo_data_map
 
   name        = each.value.name
   description = each.value.description
--- variables.tf
+++ variables.tf
@@ -1,10 +1,11 @@
 variable "github_token" {
   type        = string
+  default     = "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"
   description = "GitHub personal access token"
 }
 
 variable "repos_file" {
   type        = string
-  default     = "./repositories.json"
+  default     = "repositories.json"
   description = "The path to the JSON file containing the list of repositories to create"
 }
